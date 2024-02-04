f4dc2ba9d089695cd5b3b13e4ceb65007792e68c
Nada Hassan <engrnada@users.noreply.github.com>
2024-02-04 19:10:57 +0200

    Add files via upload

---
+++ MCAL/EXTI/EXTI_Private.h
@@ -0,0 +1,18 @@
+/*
+ * EXTI_Private.h
+ *
+ *  Created on: Dec 15, 2023
+ *      Author: Nada
+ */
+
+#ifndef EXTI_PRIVATE_H_
+#define EXTI_PRIVATE_H_
+
+#define MCUCR_REG   *((volatile u8 *)0x55)
+#define MCUCSR_REG  *((volatile u8 *)0x54)
+#define GICR_REG    *((volatile u8 *)0x5B)
+#define GIFR_REG    *((volatile u8 *)0x5A)
+
+#define NULL 0
+
+#endif /* EXTI_PRIVATE_H_ */

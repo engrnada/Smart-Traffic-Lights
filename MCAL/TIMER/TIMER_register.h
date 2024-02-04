1a34577e2738ec0c37a4984394512caf5786d325
Nada Hassan <engrnada@users.noreply.github.com>
2024-02-04 19:27:10 +0200

    Update TIMER_register.h

--- MCAL/TIMER/TIMER_register.h
+++ MCAL/TIMER/TIMER_register.h
@@ -10,16 +10,14 @@
 #ifndef TIMER_REGISTER_H_
 #define TIMER_REGISTER_H_
 
-#define SREG            *((volatile u8 *)0x5F)
+        #define SREG            *((volatile u8 *)0x5F)
 	#define TIMSK			*((volatile u8*)0X59)
 	#define TIFR			*((volatile u8*)0X58)
 
-	/*Timer0*/
 	#define TCCR0			*((volatile u8*)0X53)
 	#define TCNT0			*((volatile u8*)0X52)
 	#define OCR0			*((volatile u8*)0X5C)
 
-	/*Timer1*/
 	#define TCCR1A			*((volatile u8*)0X4F)
 	#define TCCR1B			*((volatile u8*)0X4E)
 	#define TCNT1H          *((volatile u8 *)(0x4D))
@@ -35,7 +33,6 @@
 	#define ICR1L           *((volatile u8 *)(0x46))
 	#define ICR1			*((volatile u16*)0X46)
 
-	/*Timer2*/
 
 	#define TCCR2			*((volatile u8*)0X45)
 	#define TCNT2			*((volatile u8*)0X44)

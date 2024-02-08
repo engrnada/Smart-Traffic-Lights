8a757eabee341f8737db9b9aee216eb16c75580f
Nada Hassan <engrnada@users.noreply.github.com>
2024-02-08 18:08:04 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+
+ *
+ *  Created on: Oct 13, 2022
+ *      Author: Mahmoud Karem
+ */
+
+#include "APP/APP_interface.h"
+
+int main ()
+{
+	APP_voidInit();
+	while(1);
+	return 0;
+}

77d9706117a10082e5a531834ea57326bc2e57d6
Nada Hassan <engrnada@users.noreply.github.com>
2024-02-08 18:08:56 +0200

    Create CLCD_Config.h

---
+++ HAL/CLCD/CLCD_Config.h
@@ -0,0 +1,24 @@
+/*
+ * CLCD_Config.h
+ *
+ *  Created on: Nov 23, 2023
+ *      Author: Nada
+ */
+
+#ifndef CLCD_CONFIG_H_
+#define CLCD_CONFIG_H_
+
+
+#define CLCD_Mode  4
+
+
+#define CLCD_RS    DIO_PIN5
+#define CLCD_Rw    DIO_PIN6
+#define CLCD_En    DIO_PIN7
+
+
+#define CLCD_DataPort        DIO_PORTD
+#define CLCD_ControlPort     DIO_PORTB
+
+
+#endif /* CLCD_CONFIG_H_ */
